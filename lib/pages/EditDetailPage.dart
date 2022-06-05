857c1558a2968b121a5c5dc419746b2873114264
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2022-06-05 18:59:19 +0530

    Design Edit Entry Page

--- lib/pages/EditDetailPage.dart
+++ lib/pages/EditDetailPage.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:diary/models/DiaryEntry.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../models/DairyUser.dart';
 
@@ -29,30 +31,79 @@ class _EditDetailPage extends State<EditDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Edit ${widget.diaryEntry.getTitleFromTimeStamp()}"),
+        title: Center(
+            child: Text(
+          "Edit: ${widget.diaryEntry.getTitleFromTimeStamp()}",
+          style: GoogleFonts.sourceSansPro(
+              fontWeight: FontWeight.bold, fontSize: 22),
+        )),
+        toolbarHeight: 70,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(20),
+          ),
+        ),
+        backgroundColor: Color(0xFF4b39ba),
       ),
       body: Column(
         children: [
-          TextField(
-            controller: _entryController,
-            keyboardType: TextInputType.multiline,
-            maxLines: null,
-            onChanged: (v) {
-              setState(() {
-                _entryError = "";
-              });
-            },
-            decoration: InputDecoration(
-              labelText: "Dairy entry",
-              errorText: _entryError == "" ? null : _entryError,
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(26.0, 26.0, 26.0, 100),
+            child: Material(
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              color: Color(0xFF4b39ba),
+              child: Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                  child: TextField(
+                    controller: _entryController,
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    onChanged: (v) {
+                      setState(() {
+                        _entryError = "";
+                      });
+                    },
+                    style: GoogleFonts.sourceSansPro(
+                        color: Colors.white, fontSize: 25),
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                        labelText: "Dairy entry...",
+                        errorText: _entryError == "" ? null : _entryError,
+                        labelStyle: GoogleFonts.sourceSansPro(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15)),
+                  ),
+                ),
+              ),
             ),
           ),
-          Builder(builder: (context) {
-            return ElevatedButton(
-                onPressed: () => submit(context), child: Text("SUBMIT"));
-          }),
+          Spacer(),
         ],
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Builder(builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: FloatingActionButton(
+            backgroundColor: Color(0xFF4b39ba),
+            child: FaIcon(FontAwesomeIcons.check),
+            onPressed: () => submit(context),
+          ),
+        );
+      }),
     );
   }
 
